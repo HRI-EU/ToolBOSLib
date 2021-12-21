20137f89d30af4337e177a6017a40a6e99f9a19b
Marijke Stein <marijke.stein@honda-ri.de>
2021-12-21 13:10:17 +0100

    [TBCORE-2232] initial GitLab CI configuration

---
+++ ci-test.sh
@@ -0,0 +1,32 @@
+#!/bin/bash
+#
+#  Copyright (c) Honda Research Institute Europe GmbH
+#
+#  This file is part of ToolBOSLib.
+#
+#  ToolBOSLib is free software: you can redistribute it and/or modify
+#  it under the terms of the GNU General Public License as published by
+#  the Free Software Foundation, either version 3 of the License, or
+#  (at your option) any later version.
+#
+#  ToolBOSLib is distributed in the hope that it will be useful,
+#  but WITHOUT ANY WARRANTY; without even the implied warranty of
+#  MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the
+#  GNU General Public License for more details.
+#
+#  You should have received a copy of the GNU General Public License
+#  along with ToolBOSLib. If not, see <http://www.gnu.org/licenses/>.
+#
+#
+
+
+# BashSrc files need to be ready first, see TBCORE-2267
+# set -euxo pipefail
+
+source /hri/sit/latest/DevelopmentTools/ToolBOSCore/4.0/BashSrc
+source ${SIT}/External/anaconda3/envs/common/3.9/BashSrc
+
+BST.py --test
+
+
+# EOF
